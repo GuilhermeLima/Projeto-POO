18aa9e64aaba0fe171ccfecf93bfe3ac16d30690
Guilherme Lima Santos <guilherme_limasantos@yahoo.com>
2016-10-08 10:23:41 -0300

    Voltando menu

--- main.cpp
+++ main.cpp
@@ -3,49 +3,71 @@
 #include <string>
 
 #include "ArquivosFontes.h"
+#include "Menu.h"
 
 /*
 Autores: Guilherme Lima Santos -> 22215016-1
 		 Gustavo Andretta Piedade -> 22115010-5
 		 Rafael Serikaku -> 22215036-9
+		 Fernando Frezza do Prado -> 22215018-7
 */
 
+void criarClasse(string nomeClasse, string linguagem){
+	ArquivosFontes arquivos(nomeClasse, linguagem, "");
+
+	arquivos.criarArquivo();
+}
 
 using namespace std;
 
 int main(int argc, char** argv) {
 
 	string nomeClasse, linguagem;
-	int numLinguagem;
+	int numLinguagem, conceito;
 
-	cout << "Informe o nome da classe: " << endl;
-	cin >> nomeClasse;
+	Menu mn("Escolha um conceito: ");
+	string menu_geral[4] = {"  ", "(1) Classe", "  ", "(2) Construtor"};
+	string menu_classe[4] = {"(1) Javascript", "(2) Python", "(3) C++", "ola que tal"};
 
-	cout << "Escolhe uma das linguagens a abaixo: " << endl;
-	cout << "(1) Javascript" << endl;
-	cout << "(2) Python" << endl;
-	cout << "(3) C++" << endl;
+	mn.setItens(menu_geral);
+	mn.setSubItens(1, menu_classe);
 
-	cin >> numLinguagem;
+	mn.getNome();
+	mn.getItens();
+	cin >> conceito;
 
-	switch(numLinguagem){
+	switch(conceito){
 		case 1:
-			linguagem = "js";
+			cout << "Informe o nome da classe: " << endl;
+			cin >> nomeClasse;
+			
+			cout << "Escolha uma das linguagens: " << endl;
+			mn.getSubItens(1);
+			cin >> numLinguagem;
+			
+			switch(numLinguagem){
+				case 1:
+					linguagem = "js";
+				break;
+				case 2:
+					linguagem = "py";
+				break;
+				case 3:
+					linguagem = "js";
+				break;
+				default:
+					linguagem = "js";
+			}
+			
+			criarClasse(nomeClasse, linguagem);
+	
 		break;
 		case 2:
-			linguagem = "py";
-		break;
-		case 3:
-			linguagem = "cpp";
-		break;
-		default: 
-			linguagem = "js";
+			cout << "Ainda nao tem" << endl;
+			break;
+		default:
+			mn.getSubItens(1);
 	}
-
-	ArquivosFontes arquivos(nomeClasse, linguagem, "");
-	
-	arquivos.criarArquivo();
-	//arquivos.print(); 
 
 	system("PAUSE");
 	return 0;

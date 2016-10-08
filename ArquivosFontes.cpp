9e69ce7ddcccf48a0576e9366671977bb0ad4baa
MrFrezza <frezzans@gmail.com>
2016-10-08 10:15:01 -0300

    Alteração no ArquivosFontes
    Recebe o nome da classe e altera

--- ArquivosFontes.cpp
+++ ArquivosFontes.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <fstream>
 
+#include <windows.h>
+
 #include "ArquivosFontes.h"
 
 /*
@@ -15,20 +17,22 @@ Autores: Guilherme Lima Santos -> 22215016-1
 using namespace std;
 
 //função de replace para trocar caracteres especiais dos arquivo layout
-string replace(string & texto, const string& procurado, const string& troca) {
+string replace(string& texto, const string& procurado, const string& troca) {
+	int tamanho = procurado.length();
     int inicio_procurado = texto.find(procurado); //procura o local de inicio da string no vtor de char
 
-    if(inicio_procurado == string::npos){ // verifcando se foi encontrado, se o npos retornar -1 siguinifica que nada foi encontrado
+    if(inicio_procurado == string::npos){ // verifcando se foi encontrado, se o npos retornar -1 significa que nada foi encontrado
         return string(texto);
 	}
 
-    texto.replace(inicio_procurado, procurado.length(), troca); // trocando o texto solictado,
-
+    //texto.replace(inicio_procurado, inicio_procurado, troca); // trocando o texto solicitado,
+	texto.replace(inicio_procurado, tamanho ,  troca); 
+	
     if(texto.find(procurado) != string::npos){ // verifica se após a troca ainda há mais alguma coisa
     	return replace(texto, procurado, troca); // chama a função de novo
 	}
 
-    return string(texto); // retorna a strin pronta
+    return string(texto); // retorna a string pronta
 }
 
 // contrutor da classe, o local por padrão é o local aonde o programa está arqmazenado
@@ -78,8 +82,16 @@ Função que criara  arquivo utiizando o seu arquivo layout e trocando as variavei
 */
 int ArquivosFontes::criarArquivo(){
 	string arquivo = getNomeInteiro(), linhas, layout = "layout/layout." + getExtensao();
-
-	ofstream novoArquivo; // instancia da classe ofstream para manipular arquivos
+	
+	/*string arquivoo = getNome() + ".";
+	string ext = getExtensao();
+	
+	strcat(arquivo, ext);
+	
+	char *caminho = "/cod_font";
+	strcat(caminho, );*/
+	
+	std::ofstream novoArquivo(); // instancia da classe ofstream para manipular arquivos
 	novoArquivo.open(arquivo.c_str()); // abrindo e criando arquivo
 
 	ifstream arquivoLayout(layout.c_str()); // abrindo arquivo de layout
@@ -87,8 +99,16 @@ int ArquivosFontes::criarArquivo(){
 
 		while( getline(arquivoLayout, linhas) ){ // percorando linhas do arquivo layout
 		// gravando linha a linha e trocando conteudos especiais do arquivo layout
-		  novoArquivo << replace(linhas, "$[nome_classe]", getNome()) << endl;
+		  novoArquivo << replace(linhas, "nome_classe", getNome()) << endl;
+
+
+		//Exibe cada liha sendo escrita no arquivo
+		 cout<<"---> "<<linhas<<endl;
+		 Sleep(500);//Timer na escrita, 0.5 segundos
+		  
 		}
+		
+		cout<<"\n";
 		//fechando arquivo layout
 		arquivoLayout.close();
 	}
--- main.cpp
+++ main.cpp
@@ -3,71 +3,49 @@
 #include <string>
 
 #include "ArquivosFontes.h"
-#include "Menu.h"
 
 /*
 Autores: Guilherme Lima Santos -> 22215016-1
 		 Gustavo Andretta Piedade -> 22115010-5
 		 Rafael Serikaku -> 22215036-9
-		 Fernando Frezza do Prado -> 22215018-7
 */
 
-void criarClasse(string nomeClasse, string linguagem){
-	ArquivosFontes arquivos(nomeClasse, linguagem, "");
-
-	arquivos.criarArquivo();
-}
 
 using namespace std;
 
 int main(int argc, char** argv) {
 
 	string nomeClasse, linguagem;
-	int numLinguagem, conceito;
+	int numLinguagem;
 
-	Menu mn("Escolha um conceito: ");
-	string menu_geral[4] = {"  ", "(1) Classe", "  ", "(2) Construtor"};
-	string menu_classe[4] = {"(1) Javascript", "(2) Python", "(3) C++", "ola que tal"};
+	cout << "Informe o nome da classe: " << endl;
+	cin >> nomeClasse;
 
-	mn.setItens(menu_geral);
-	mn.setSubItens(1, menu_classe);
+	cout << "Escolhe uma das linguagens a abaixo: " << endl;
+	cout << "(1) Javascript" << endl;
+	cout << "(2) Python" << endl;
+	cout << "(3) C++" << endl;
 
-	mn.getNome();
-	mn.getItens();
-	cin >> conceito;
+	cin >> numLinguagem;
 
-	switch(conceito){
+	switch(numLinguagem){
 		case 1:
-			cout << "Informe o nome da classe: " << endl;
-			cin >> nomeClasse;
-			
-			cout << "Escolha uma das linguagens: " << endl;
-			mn.getSubItens(1);
-			cin >> numLinguagem;
-			
-			switch(numLinguagem){
-				case 1:
-					linguagem = "js";
-				break;
-				case 2:
-					linguagem = "py";
-				break;
-				case 3:
-					linguagem = "js";
-				break;
-				default:
-					linguagem = "js";
-			}
-			
-			criarClasse(nomeClasse, linguagem);
-	
+			linguagem = "js";
 		break;
 		case 2:
-			cout << "Ainda nao tem" << endl;
-			break;
-		default:
-			mn.getSubItens(1);
+			linguagem = "py";
+		break;
+		case 3:
+			linguagem = "cpp";
+		break;
+		default: 
+			linguagem = "js";
 	}
+
+	ArquivosFontes arquivos(nomeClasse, linguagem, "");
+	
+	arquivos.criarArquivo();
+	//arquivos.print(); 
 
 	system("PAUSE");
 	return 0;
